b4c2700583091e8cb24475fa443f342e968671cd
Maximilian Budziat <maximilian.budziat@scheer-group.com>
2023-02-28 17:26:56 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useState } from "react";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  style: ["normal"],
+  weight: ["100", "300", "400", "500", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [post, setPost] = useState([]);
+
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily}, sans-serif;
+        }
+      `}</style>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/testsite">Testsite</Link>
+          </li>
+        </ul>
+      </nav>
+      <Component {...pageProps} post={post} setPost={setPost} />
+    </>
+  );
 }
